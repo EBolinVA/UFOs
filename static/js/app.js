bd02bf3c142b19786e821c7f5b7088cbd2c74810
EBolinVA <elizabeth.bolin@gmail.com>
2023-01-23 21:39:37 -0500

    update .js files

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+// import the table from data.js
+const tableData = data;
+// Reference thte HTML table using d3
+var tbody = d3.select("tbody");
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!!")
+}
+
+// Takes two numbers and adds them 
+function addition(a, b) {
+    return a + b;
+}
